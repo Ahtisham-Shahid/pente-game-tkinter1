b47dc3028bf78408670260eb3b88b9e0f9914276
Ayesha-kiran1 <ayeshaa6811@gmail.com>
2025-07-07 01:22:37 +0500

    Update result_screen.py

--- result_screen.py
+++ result_screen.py
@@ -1,2 +1,16 @@
 # Created by Ayesha
 # Task: Game
+# Code
+import tkinter as tk
+
+def show_result(winner):
+    result = tk.Tk()
+    result.title("Game Over")
+
+    label = tk.Label(result, text=f"🏆 Player {winner} Wins!", font=("Arial", 20))
+    label.pack(pady=20)
+
+    btn = tk.Button(result, text="Restart Game", command=result.destroy)
+    btn.pack(pady=10)
+
+    result.mainloop()
