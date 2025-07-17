93556fdd8cdd68275def1a0d88309aeec35e0797
axnafeesaa <nafeesaali253@gmail.com>
2025-07-17 18:18:14 +0500

    Update win-logic.py

--- win-logic.py
+++ win-logic.py
@@ -1,12 +1,13 @@
-from game_board import PenteGame
+from game_board import PenteGame, SIZE
 
 def check_five_in_a_row(board, x, y, color):
     directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
+    
     for dx, dy in directions:
         count = 1
         for direction in [1, -1]:
             nx, ny = x + direction * dx, y + direction * dy
-            while 0 <= nx < 13 and 0 <= ny < 13 and board[nx][ny] == color:
+            while 0 <= nx < SIZE and 0 <= ny < SIZE and board[nx][ny] == color:
                 count += 1
                 nx += direction * dx
                 ny += direction * dy
